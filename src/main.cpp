fe5c9033e2f05826e9e22ed654ca07d834f15324
judgeou <judgeou@gamil.com>
2024-11-08 01:30:13 +0800

    blank window

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,66 @@
-int main() {
+#include <imgui.h>
+#include <Windows.h>
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavutil/avutil.h>
+}
+
+// 窗口过程函数声明
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPSTR lpCmdLine,
+    _In_ int nCmdShow
+) {
+    // 注册窗口类
+    WNDCLASSEX wc = {};
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = L"VideoPlayerClass";
+    
+    RegisterClassEx(&wc);
+
+    // 创建窗口
+    HWND hwnd = CreateWindowEx(
+        0,
+        L"VideoPlayerClass",
+        L"视频播放器",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        800, 600,
+        nullptr,
+        nullptr,
+        hInstance,
+        nullptr
+    );
+
+    if (hwnd == nullptr) {
+        return 1;
+    }
+
+    // 显示窗口
+    ShowWindow(hwnd, SW_SHOW);
+
+    // 消息循环
+    MSG msg = {};
+    while (GetMessage(&msg, nullptr, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
     return 0;
+}
+
+// 窗口过程函数实现
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
